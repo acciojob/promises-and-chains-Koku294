02cdb979f9bde07f73b4a31e1c2f94f4d234c442
Koku294 <90713556+Koku294@users.noreply.github.com>
2025-06-03 10:47:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-//your JS code here. If required.
+document.getElementById("votingform").addEventListener("Submit",function (e)
+{
+	e.preventDefault();
+	const name=document.getElementById("name").value.trim();
+	const age=document.getElementById("age").value.trim();
+
+	if(!name||!age){
+		alert("Please return valid details.");
+		return;
+	}
+const agenum=Number(age);
+
+	const checkeligibility=new Promise((resolve,reject)=>{
+		setTimeout(()=>{
+			if(agenum>18){
+				resolve("Welcome, ${name}. you can vote.");
+			}
+			else{
+				reject("Oh sorry, ${name}. You are not old enough.");
+			}
+			
+		},4000);
+	});
+	checkeligibility.then(message=>alert(message))
+	.catch(error=>alert(error));
+									   });
+	
+}
